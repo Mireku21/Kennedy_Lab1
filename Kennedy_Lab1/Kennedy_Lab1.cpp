8d08298878856c541a3111f82a5925ded39e10a5
kmireku <kennedymireku3@gmail.com>
2026-04-08 11:41:37 -0500

    v

--- Kennedy_Lab1/Kennedy_Lab1.cpp
+++ Kennedy_Lab1/Kennedy_Lab1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello from Kennedy! This is my first Git commit by Kennedy." << endl;
+    cout << "Hello from Kennedy! This is my first Git commit by Kennedy..." << endl;
     cout << "Git makes collaboration easy! " << endl;
     cout << "Lab 1 completed on 4/8/2026!" << endl;
     cout << "Feature by Kennedy : Branching is awesome!" << endl;
